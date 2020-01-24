35e969236e647a7a05b846f48f9948f5dd7b9f07
JunH-K <k.developer@kakao.com>
2020-01-24 20:43:45 +0900

    [update] 투표 결과화면

--- src/containers/result/VoteResultContainer.jsx
+++ src/containers/result/VoteResultContainer.jsx
@@ -2,38 +2,48 @@ import React from 'react';
 import styled, { keyframes } from 'styled-components';
 import { Button } from 'antd';
 import { Link } from 'react-router-dom';
+import useStore from '../../store/useStore';
 
 const VoteResultContainer = props => {
+  const { getResult } = useStore();
   const {
     match: {
       params: { id },
     },
+    history,
   } = props;
-  const per = 50;
-  console.log(`결과 id ${id}`);
-  return (
+  const { totalVoter, items } = getResult(id);
+
+  !(totalVoter && items) && history.push('/');
+
+  return totalVoter && items ? (
     <>
       <div className="graph_container">
-        <div className={'graph'}>
-          <SpanGraph width={per}>{per}%</SpanGraph>
-        </div>
-
-        <div className={'graph'}>
-          <SpanGraph width={per}>{per}%</SpanGraph>
-        </div>
-
-        <div className={'graph'}>
-          <SpanGraph width={per}>{per}%</SpanGraph>
-        </div>
+        {items.map((item, index) => {
+          return (
+            <>
+              <div className="result_title">
+                {`${index + 1}. `}
+                {item.title}
+                {` (${item.votes}표)`}
+              </div>
+              <div className={'graph'}>
+                <SpanGraph width={item.percentage} index={index}>
+                  {item.percentage}%
+                </SpanGraph>
+              </div>
+            </>
+          );
+        })}
       </div>
-      <div>전체투표수:~</div>
+      <div>전체투표수 : {totalVoter}</div>
       <div className={'first'}>
         <Link to={'/'}>
           <Button>처음으로</Button>
         </Link>
       </div>
     </>
-  );
+  ) : null;
 };
 
 const stack = width => {
@@ -53,7 +63,8 @@ const SpanGraph = styled.span`
   width: ${({ width }) => width}%;
   line-height: 40px;
   text-align: right;
-  background: pink;
+  background: ${({ index }) =>
+    index % 2 === 0 ? 'LightSkyBlue' : 'pink'};
   border-radius: 40px;
   box-sizing: border-box;
   animation: ${({ width }) => stack(width)} 1s 1;
