af21ff20c0b0860034ceaaf227e522bfd96a7c1a
JunH-K <k.developer@kakao.com>
2020-01-25 13:38:07 +0900

    [update] List item 컴포넌트분리, 로그인 붙임

--- src/containers/votesList/VoteListContainer.jsx
+++ src/containers/votesList/VoteListContainer.jsx
@@ -1,57 +1,113 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { Button } from 'antd';
 import useStore from '../../store/useStore';
 
 const VoteListContainer = ({ match, history }) => {
-  const { getVotes } = useStore();
+  const { getVotes, getLoginUser, logout } = useStore();
   const votes = getVotes();
-  const user = '누가만들었음';
+  const user = getLoginUser();
+
+  useEffect(() => {
+    if (!Object.entries(user).length) {
+      history.push('/login');
+    }
+  });
 
   const onClickDelete = index => event => {
     event.stopPropagation();
-    console.log(`삭제${index}`);
   };
 
   const onClickEdit = index => event => {
     event.stopPropagation();
-    console.log(`수정${index}`);
   };
 
   const startVote = index => () => {
     history.push(`/votes/${index}`);
   };
 
+  const onClickCreateVote = () => {
+    history.push(`/create`);
+  };
+
+  const onClickLogout = () => {
+    logout(() => {
+      history.push(`/`);
+    });
+  };
+
   return (
     <>
-      <div className="contents">
-        {votes.map((vote, index) => {
-          return (
-            <div className="item" onClick={startVote(index)} key={index}>
-              <h3>{vote.voteTitle}</h3>
-              <p>생성자:{vote.creator}</p>
-              <p>기간:{vote.votePeriod.toString()}</p>
-              <p>진행중:진행중</p>
-              {vote.creator === user && (
-                <div className="btn_wrap">
-                  <Button
-                    className={'btn btn_delete'}
-                    onClick={onClickDelete(index)}
-                  >
-                    삭제
-                  </Button>
-                  <Button
-                    className={'btn btn_edit'}
-                    onClick={onClickEdit(index)}
-                  >
-                    수정
-                  </Button>
-                </div>
-              )}
-            </div>
-          );
-        })}
+      <div style={{ textAlign: 'right', margin: '10px' }}>
+        <Button type="primary" onClick={onClickCreateVote}>
+          투표 생성하기
+        </Button>
+        <Button
+          type="primary"
+          style={{ marginLeft: '10px' }}
+          onClick={onClickLogout}
+        >
+          로그아웃
+        </Button>
       </div>
+      {!votes.length && '투표가 아직 없습니다. 생성하세요.'}
+      {user ? (
+        <div className="contents">
+          {votes.map((vote, index) => {
+            return (
+              <VoteListItem
+                key={vote.creator}
+                vote={vote}
+                startVote={startVote}
+                index={index}
+                user={user}
+                onClickDelete={onClickDelete(index)}
+                onClickEdit={onClickEdit(index)}
+              />
+            );
+          })}
+        </div>
+      ) : null}
     </>
+  );
+};
+
+const VoteListItem = ({
+  vote,
+  startVote,
+  index,
+  user,
+  onClickDelete,
+  onClickEdit,
+}) => {
+  console.log(vote);
+  console.log(user);
+  return (
+    <div className="item" onClick={startVote(index)} key={index}>
+      <h3 style={{ margin: '5px', borderBottom: '1px solid black' }}>
+        {vote.voteTitle}
+      </h3>
+      <p>생성자 : {vote.creator}</p>
+      <p>기간 : {vote.votePeriod.toString()}</p>
+      <p>진행중 : 진행중</p>
+      {vote.creator === user.name && (
+        <div className="btn_wrap">
+          <Button
+            type="primary"
+            className={'btn btn_delete'}
+            onClick={onClickDelete}
+          >
+            삭제
+          </Button>
+          <Button
+            type="primary"
+            className={'btn btn_edit'}
+            onClick={onClickEdit}
+          >
+            수정
+          </Button>
+        </div>
+      )}
+    </div>
   );
 };
 
