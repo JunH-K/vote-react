cfbc7d759eb42562d55e26b18759728ef49b7179
JunH-K <k.developer@kakao.com>
2020-01-27 00:30:09 +0900

    [update] 투표기간 및 투표유무 추가

--- src/containers/vote/VoteContainer.jsx
+++ src/containers/vote/VoteContainer.jsx
@@ -1,17 +1,43 @@
-import React, { useCallback, useState } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 import { Button } from 'antd';
 import useStore from '../../store/useStore';
+import moment from 'moment';
 
 const VoteContainer = ({ match, history }) => {
   const [selectedItem, setSelectedItem] = useState(-1);
   const {
     params: { id },
   } = match;
-
-  const { getVotes, updateVote } = useStore();
+  const { getVotes, updateVote, getLoginUser } = useStore();
   const [vote] = getVotes(id);
+  const user = getLoginUser();
 
   !vote && history.push('/');
+
+  useEffect(() => {
+    if (vote) {
+      const { name } = user;
+      const { voter } = vote;
+      if (voter.includes(name)) {
+        alert('이미 투표 했습니다.');
+        history.push(`/result/${id}`);
+      }
+    }
+  });
+
+  useEffect(() => {
+    if (vote) {
+      const isBetween = moment().isBetween(
+        moment(vote.votePeriod[0]),
+        moment(vote.votePeriod[1])
+      );
+
+      if (!isBetween) {
+        alert('투표 기간이 아닙니다.');
+        history.push(`/result/${id}`);
+      }
+    }
+  });
 
   const onChange = useCallback(
     index => () => {
@@ -21,10 +47,10 @@ const VoteContainer = ({ match, history }) => {
   );
 
   const onClickVote = useCallback(() => {
-    updateVote(id, selectedItem, '투표자11', () =>
+    updateVote(id, selectedItem, user.name, () =>
       history.push(`/result/${id}`)
     );
-  }, [history, id, updateVote, selectedItem]);
+  }, [history, id, updateVote, selectedItem, user.name]);
 
   const isSelected = selectedItem > -1;
 
@@ -32,7 +58,9 @@ const VoteContainer = ({ match, history }) => {
     <>
       {vote ? (
         <div className="vote_container">
-          <div className="vote_title"><h2>{vote.voteTitle}</h2></div>
+          <div className="vote_title">
+            <h2>{vote.voteTitle}</h2>
+          </div>
           <div className="vote_List">
             {vote.items.map((item, index) => {
               return (
