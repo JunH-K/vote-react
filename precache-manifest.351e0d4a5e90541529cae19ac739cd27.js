bbc81ba991bd9d7691aa176043ac8a5982cef190
JunH-K <k.developer@kakao.com>
2020-01-27 15:12:32 +0900

    Updates

---
+++ precache-manifest.351e0d4a5e90541529cae19ac739cd27.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "343ec11ed735ba2c54419706dca5819d",
+    "url": "/vote-react-deploy/index.html"
+  },
+  {
+    "revision": "522ecbd3bf57d4ac2141",
+    "url": "/vote-react-deploy/static/css/2.ff8bd605.chunk.css"
+  },
+  {
+    "revision": "0ed24c7aeae37c5a0d45",
+    "url": "/vote-react-deploy/static/css/main.c91516ed.chunk.css"
+  },
+  {
+    "revision": "522ecbd3bf57d4ac2141",
+    "url": "/vote-react-deploy/static/js/2.8ad7739f.chunk.js"
+  },
+  {
+    "revision": "86afaa925e0f432774bf95b19b6933fb",
+    "url": "/vote-react-deploy/static/js/2.8ad7739f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ed24c7aeae37c5a0d45",
+    "url": "/vote-react-deploy/static/js/main.93445af0.chunk.js"
+  },
+  {
+    "revision": "f97c6e9764af370a72e4",
+    "url": "/vote-react-deploy/static/js/runtime-main.58105edf.js"
+  }
+]);
